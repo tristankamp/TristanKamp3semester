aca8af0991b05f9ea157c32fd716dd068f6f87a0
tristankamp <52030187+tristankamp@users.noreply.github.com>
2020-08-31 09:22:31 +0200

    Add files via upload

---
+++ student/student/Student.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace student
+{
+    public class Student
+    {
+        public enum GenderType { male, female }
+        private string _address;
+        private string _name;
+        private int _semester;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value.Length > 1)
+                    _name = value;
+                throw new ArgumentException();
+
+            }
+            
+        }
+
+        public string Address
+        {
+            get => _address;
+            set
+            {
+                if (value != null)
+                    _address = value;
+                else throw new ArgumentNullException();
+            
+            }
+        }
+
+        public int Semester
+        {
+            get => _semester;
+            set
+            {
+                if (value >0 && value<9)
+                    _semester = value;
+                else throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public GenderType Gender
+        {
+            get;
+            set;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ student/studentTests/StudentTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using student;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace student.Tests
+{
+    [TestClass()]
+    public class StudentTests
+    {       
+        //arrange
+
+        Student mig = new Student();
+
+
+        [TestMethod()]
+
+        //act
+
+        //assert
+
+        public void ToStringTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
